f3704405fd397ed59e0c4a011e9b3a730c7e5f8e
Brent Vervaet <brent.vervaet@student.hogent.be>
2025-08-19 16:44:14 +0200

    model

---
+++ RealDealMeal/Models/Meal.swift
@@ -0,0 +1,21 @@
+//
+//  Meal.swift
+//  RealDealMeal
+//
+//  Created by Brent Vervaet on 19/08/2025.
+//
+
+import Foundation
+
+struct MealResponse: Codable {
+	let meals: [Meal]?
+}
+
+struct Meal: Codable, Identifiable {
+	let idMeal: String
+	let strMeal: String
+	let strMealThumb: String
+	let strInstructions: String?
+	
+	var id: String { idMeal }
+}
