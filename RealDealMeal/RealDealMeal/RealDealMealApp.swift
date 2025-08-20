20105746d6b73b91352c7648bc66ec033b7eeb55
Brent Vervaet <brent.vervaet@student.hogent.be>
2025-08-20 13:47:40 +0200

    change App

--- RealDealMeal/RealDealMeal/RealDealMealApp.swift
+++ RealDealMeal/RealDealMeal/RealDealMealApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct RealDealMealApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			MealListView()
+		}
+	}
 }
---
+++ RealDealMeal/ViewModels/FavoritesViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesViewModel.swift
+//  RealDealMeal
+//
+//  Created by Brent Vervaet on 20/08/2025.
+//
+
+import SwiftUI
+
+struct FavoritesViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesViewModel()
+}
