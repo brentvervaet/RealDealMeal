65d3079653afa2821e25d03da81c538406b06fa3
Brent Vervaet <brent.vervaet@student.hogent.be>
2025-08-20 19:41:20 +0200

    UI change + cleaner code

--- RealDealMeal/Views/MealListView.swift
+++ RealDealMeal/Views/MealListView.swift
@@ -16,52 +16,18 @@ struct MealListView: View {
 		NavigationStack {
 			VStack {
 				searchBar
-				
 				if let errorMessage = mealListVM.errorMessage {
 					Text(errorMessage)
 						.foregroundColor(.red)
 						.padding()
 				} else {
-					ScrollView {
-						LazyVStack(spacing: 12) {
-							ForEach(mealListVM.meals) { meal in
-								NavigationLink(destination: MealDetailView(meal: meal)) {
-									HStack(spacing: 16) {
-										AsyncImage(url: URL(string: meal.strMealThumb)) { image in
-											image.resizable()
-												.scaledToFill()
-										} placeholder: {
-											Color.gray.opacity(0.3)
-										}
-										.frame(width: 80, height: 80)
-										.clipShape(RoundedRectangle(cornerRadius: 12))
-										
-										Text(meal.strMeal)
-											.font(.headline)
-											.foregroundColor(.primary)
-										
-										Spacer()
-									}
-									.padding()
-									.background(Color(.systemBackground))
-									.cornerRadius(12)
-									.overlay(
-										RoundedRectangle(
-											cornerRadius: 12,
-										)
-										.stroke(.gray.opacity(0.1))
-									)
-									.shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
-								}
-							}
-						}
-						.padding()
-					}
+				mealList
 				}
 			}
 			.navigationTitle("Find a recipe")
 		}
 	}
+	
 	private var searchBar: some View {
 		HStack {
 			TextField("Search meals...", text: $mealListVM.searchQuery)
@@ -76,6 +42,44 @@ struct MealListView: View {
 		}
 		.padding(.horizontal)
 		.padding(.top)
+	}
+	
+	private var mealList: some View {
+		ScrollView {
+			LazyVStack(spacing: 12) {
+				ForEach(mealListVM.meals) { meal in
+					NavigationLink(destination: MealDetailView(meal: meal)) {
+						HStack(spacing: 16) {
+							AsyncImage(url: URL(string: meal.strMealThumb)) { image in
+								image.resizable()
+									.scaledToFill()
+							} placeholder: {
+								Color.gray.opacity(0.3)
+							}
+							.frame(width: 80, height: 80)
+							.clipShape(RoundedRectangle(cornerRadius: 12))
+							
+							Text(meal.strMeal)
+								.font(.headline)
+								.foregroundColor(.primary)
+							
+							Spacer()
+						}
+						.padding()
+						.background(Color(.systemBackground))
+						.cornerRadius(12)
+						.overlay(
+							RoundedRectangle(
+								cornerRadius: 12,
+							)
+							.stroke(.gray.opacity(0.1))
+						)
+						.shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
+					}
+				}
+			}
+			.padding()
+		}
 	}
 }
 
